8389065e095d5257a79f0f586d8b0fe6411c3d91
ryuken <ryuken73@gmail.com>
2025-09-10 15:18:23 +0900

    fix: docking area should occupy size

--- src/renderer/Components/Assets/AssetViewer.jsx
+++ src/renderer/Components/Assets/AssetViewer.jsx
@@ -42,10 +42,8 @@ const options = {
 };
 
 const Container = styled.div`
-  // width: 100%;
-  // height: 100%;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   touch-action: none;
   user-select: none;
 `;
